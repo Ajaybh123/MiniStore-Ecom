f62e44e7fcc2126c13b00fe122df7d4babfa6747
Ajay Bhardwaj <ajayb0663@gmail.com>
2024-08-16 17:06:17 +0530

    I used redux on my project mini store

---
+++ src/Redux/Reducers/MaincategoryReducer.jsx
@@ -0,0 +1,25 @@
+import { ADD_MIANCATEGORY_RED, DELETE_MIANCATEGORY_RED, GET_MIANCATEGORY_RED, UPDATE_MIANCATEGORY_RED } from "../Constants";
+
+export default function MaincategoryReducer(state=[],action){
+    let newState,index
+    switch(action.type){
+        case ADD_MIANCATEGORY_RED:
+            newState = [...state]
+            newState.push(action.payload)
+            return newState
+
+        case GET_MIANCATEGORY_RED:
+            return action.payload
+
+        case UPDATE_MIANCATEGORY_RED:
+            index = state.find((x)=>x.id===action.payload.id)
+            state[index].name = action.payload.name
+            return state
+
+        case DELETE_MIANCATEGORY_RED:
+            return state.filter(x=>x.id !== action.payload.id)
+
+        default:
+            return state
+    }
+}
